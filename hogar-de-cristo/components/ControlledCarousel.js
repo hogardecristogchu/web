86e24858c3372746921e9c3ff4ac3d96f0027d03
Raquel Muriega <muriega.raquel@gmail.com>
2021-11-01 17:24:32 -0300

    carousel

---
+++ hogar-de-cristo/components/ControlledCarousel.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const images = [
+  "https://images.unsplash.com/photo-1516655855035-d5215bcb5604?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60",
+  "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60",
+  "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60",
+  "https://images.unsplash.com/photo-1458668383970-8ddd3927deed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60",
+];
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      {images.map((image, index) => (
+        <Carousel.Item key={index}>
+          <img className="d-block w-100" height={500} src={image} alt="First slide" />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
+
+export default ControlledCarousel;
--- hogar-de-cristo/pages/index.js
+++ hogar-de-cristo/pages/index.js
@@ -1,5 +1,15 @@
+import ControlledCarousel from "../components/ControlledCarousel";
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
-  }
-  
-  export default HomePage
+  return (
+    <div>
+      <h2 style={{ widht:'100%', textAlign:'center', marginTop:'10px' }}>Welcome to Next.js!</h2>
+
+      <div style={{ width: '70%', margin: '3% auto'}}>
+        <ControlledCarousel></ControlledCarousel>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
