70112561610591fe63d786b136b6787fb724072c
Raquel Muriega <muriega.raquel@gmail.com>
2021-11-01 18:05:27 -0300

    slider

--- hogar-de-cristo/pages/index.js
+++ hogar-de-cristo/pages/index.js
@@ -3,9 +3,7 @@ import ControlledCarousel from "../components/ControlledCarousel";
 function HomePage() {
   return (
     <div>
-      <h2 style={{ widht:'100%', textAlign:'center', marginTop:'10px' }}>Welcome to Next.js!</h2>
-
-      <div style={{ width: '70%', margin: '3% auto'}}>
+      <div>
         <ControlledCarousel></ControlledCarousel>
       </div>
     </div>
