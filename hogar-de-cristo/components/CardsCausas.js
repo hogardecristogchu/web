0c2d4fb940425ae010e3a61a0459ffb176c5b325
Lourdes Luna <67447466+lourdeslunaweb@users.noreply.github.com>
2021-11-02 13:14:56 -0300

    Trabajando en: clonado de repo, creación de rama, se pasó las cards de repo local a repo remoto de hogar de cristo, creación de sección voluntariado

---
+++ hogar-de-cristo/components/CardsCausas.js
@@ -0,0 +1,96 @@
+import React from 'react'
+
+const CardsCausas = () => {
+    return (
+        <>
+            <h2 className="text-center mt-3">Nuestras principales causas</h2>
+            <h4 className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+            <div className="container mt-3 d-flex flex-wrap justify-content-evenly">
+                <div className="card my-3 mx-3" style={{ width: '20rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Consumo problemático</h5>
+                        <div class="text-center">
+                            <img src="https://w7.pngwing.com/pngs/822/995/png-transparent-square-white-and-black-logo-brand-angle-square-font-buffer-angle-social-media-square.png" class="card-img-top mt-3" alt="log" />
+                        </div>
+                        <p className="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+                <div className="card my-3 mx-3" style={{ width: '20rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Personas en situación de calle</h5>
+                        <div className="text-center">
+                            <img src="https://w7.pngwing.com/pngs/822/995/png-transparent-square-white-and-black-logo-brand-angle-square-font-buffer-angle-social-media-square.png" class="card-img-top mt-3" alt="log" />
+                        </div>
+                        <p className="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+                <div className="card my-3 mx-3" style={{ width: '20rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Reinserción educativa</h5>
+                        <div className="text-center">
+                            <img src="https://w7.pngwing.com/pngs/822/995/png-transparent-square-white-and-black-logo-brand-angle-square-font-buffer-angle-social-media-square.png" class="card-img-top mt-3" alt="log" />
+                        </div>
+                        <p className="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+                <div className="card my-3 mx-3" style={{ width: '20rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Inclusión laboral</h5>
+                        <div className="text-center">
+                            <img src="https://w7.pngwing.com/pngs/822/995/png-transparent-square-white-and-black-logo-brand-angle-square-font-buffer-angle-social-media-square.png" class="card-img-top mt-3" alt="log" />
+                        </div>
+                        <p className="card-text mt-3">Lorem ipsum dolor ect jsjs kjanajas jshshs ajanbas ajana .</p>
+                    </div>
+                </div>
+                <div className="card my-3 mx-3" style={{ width: '20rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Empoderamiento femenino</h5>
+                        <div className="text-center">
+                            <img src="https://w7.pngwing.com/pngs/822/995/png-transparent-square-white-and-black-logo-brand-angle-square-font-buffer-angle-social-media-square.png" class="card-img-top mt-3" alt="log" />
+                        </div>
+                        <p className="card-text mt-3">Empoderamiento de  mujeres que atraviesan situaciones de violencia.</p>
+                    </div>
+                </div>
+                <div className="card my-3 mx-3" style={{ width: '20rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Infancias contenidas</h5>
+                        <div className="text-center">
+                            <img src="https://w7.pngwing.com/pngs/822/995/png-transparent-square-white-and-black-logo-brand-angle-square-font-buffer-angle-social-media-square.png" class="card-img-top mt-3" alt="log" />
+                        </div>
+                        <p className="card-text mt-3">Protección de los derechos de los niños y contención de la infancia vulnerada.</p>
+                    </div>
+                </div>
+                <div className="card my-3 mx-3" style={{ width: '20rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Revinculación familiar</h5>
+                        <div className="text-center">
+                            <img src="https://w7.pngwing.com/pngs/822/995/png-transparent-square-white-and-black-logo-brand-angle-square-font-buffer-angle-social-media-square.png" class="card-img-top mt-3" alt="log" />
+                        </div>
+                        <p className="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+                <div className="card my-3 mx-3" style={{ width: '20rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Acompañamiento en contextos de encierro</h5>
+                        <div className="text-center">
+                            <img src="https://w7.pngwing.com/pngs/822/995/png-transparent-square-white-and-black-logo-brand-angle-square-font-buffer-angle-social-media-square.png" class="card-img-top mt-3" alt="log" />
+                        </div>
+                        <p className="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+                <div className="card my-3 mx-3" style={{ width: '20rem' }}>
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Cultura del encuentro</h5>
+                        <div className="text-center">
+                            <img src="https://w7.pngwing.com/pngs/822/995/png-transparent-square-white-and-black-logo-brand-angle-square-font-buffer-angle-social-media-square.png" class="card-img-top mt-3" alt="log" />
+                        </div>
+                        <p className="card-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default CardsCausas
---
+++ hogar-de-cristo/components/Voluntariado.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Voluntariado = () => {
+    return (
+        <>
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <img src="https://www.lapalabraisraelita.cl/backend/views/img/articulos/fe1f6c31eb77b.jpg" class="img-fluid" alt="voluntarios" />
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="row"><h2>Voluntariado</h2></div>
+                        <div className="row"><h4>La familia sigue creciendo</h4></div>
+                        <div className="row"><h5>Somos un equipo sin fines de lucro y trabajamos en todo el mundo.</h5></div>
+                        <div className="row"><p>Formar parte de la familia del Hogar de Cristo siendo voluntario es una experiencia sumamente enriquecedora. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></div>
+                        <div className="row"><button>Sumate a esta gran familia</button></div>
+                    </div>
+                </div>
+            </div>
+            <div className="container my-5">
+                <div className="row"><h3>Testimonios</h3></div>
+                <div className="row mt-1">
+                    <div class="card">
+                        <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p>Marcelo, 28 años</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-1">
+                    <div class="card">
+                        <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p>Juana, 53 años</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-1">
+                    <div class="card">
+                        <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <p>Mariana, 23 años</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Voluntariado
